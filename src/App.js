6b02369e13d43ad95fb97e7d6f06e116fca8609f
tinsur <tinsur@gmail.com>
2024-04-21 21:01:55 +0300

    Задание №3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let NUMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    let manager = ['+', '-', 'C', '='];
+
+    let [operand1, setOperand1] = useState('');
+    let [operand2, setOperand2] = useState('');
+    let [operator, setOperator] = useState('');
+    let [screen, setScreen] = useState('')
+    let [activeBtn, setActiveBtn] = useState('')
+
+    const clickBtnDigit = (event) => {
+        let idDigit = event.target.dataset.id;
+        if (!operator) {
+            if (operand1.length === 0 && idDigit === String(0)) return;
+            setOperand1(operand1 + idDigit)
+        } else {
+            if (operand2.length === 0 && idDigit === String(0)) return;
+            setOperand2(operand2 + idDigit)
+        }
+        setScreen(screen + idDigit)
+        setActiveBtn(idDigit)
+    }
+    const clickBtnOperator = (event) => {
+        let idOperator = event.target.dataset.id;
+
+        switch (idOperator) {
+            case '=':
+                let x = Number(operand1);
+                let y = Number(operand2)
+                let result = operator === '+' ? x + y : x - y;
+                setOperand1(String(result));
+                setScreen(result);
+                setOperator('');
+                setOperand2('');
+                setActiveBtn('')
+                break;
+            case 'C':
+                setOperand1('');
+                setOperand2("");
+                setOperator('')
+                setScreen('');
+                setActiveBtn('')
+
+                break;
+            default:
+                if (!operator&&operand1) {
+                    setScreen(screen + idOperator)
+                    setOperator(idOperator);
+
+                }
+                break;
+        }
+    }
+
+
+    return (
+        <div className="App">
+            <div className={styles.wrap}>
+                <div className={styles.resultFild}>{screen ? screen : 0}</div>
+                <ul className={styles['nums-btn']}>
+
+                    {NUMS.map((item) =>
+                        <li className={activeBtn === String(item) ? `${styles.btn} ${styles.activ}` : styles.btn}
+                            key={item}
+                            data-id={item}
+                            onClick={clickBtnDigit}>
+                            {item}</li>)
+                    }
+                </ul>
+                <ul className={styles['manager-btn']}>
+                    {manager.map((item) =>
+                        <li className={`${styles.btn} ${styles.btnM}`}
+                            key={item}
+                            data-id={item}
+                            onClick={clickBtnOperator}>
+                            {item}</li>)
+                    }
+
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
